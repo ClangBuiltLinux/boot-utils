9d37a7b70f50cde3d317b29dd6402cb12e1c19fa
Nathan Chancellor <nathan@kernel.org>
2022-03-23 12:51:36 -0700

    boot-uml.sh: Add link to issue for 'exec' workaround
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- boot-uml.sh
+++ boot-uml.sh
@@ -30,7 +30,8 @@ function decomp_rootfs() {
 }
 
 function execute_kernel() {
-    # exec is needed to avoid a "Killed" message when the kernel shuts down.
+    # exec is needed to avoid a "Killed" message when the kernel shuts down:
+    # https://github.com/ClangBuiltLinux/boot-utils/issues/60
     # Nothing runs after this command so it is okay for it to replace this process.
     exec "$KERNEL" ubd0="$rootfs" "${kernel_args[@]}"
 }
